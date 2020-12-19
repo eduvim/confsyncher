ba3a2bc379b2b8899df429984220c9618757634f
Eduardo Villaseñor <evillasrmx@gmail.com>
2020-12-18 22:30:28 -0600

    Adds file exists scenario

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Definition struct {
@@ -18,55 +18,56 @@ type Tool struct {
 }
 
 func main() {
-	// validate file
 	args := os.Args
-	fmt.Println(args[0])
 	files, err := ioutil.ReadDir(args[1])
-	if IsNotNil(err){
-		log.Fatalf("cannot open the declared dir %s %+v" , args[1] , err)
+	if IsNotNil(err) {
+		log.Fatalf("cannot open the declared dir %s %+v", args[1], err)
 	}
+
 	data := ""
-	for _ , file := range files{
-		if file.Name() == "definitions.yml"{
-			bytes , err := ioutil.ReadFile(os.Args[1] + "/definitions.yml")
+	for _, file := range files {
+		if file.Name() == "definitions.yml" {
+			bytes, err := ioutil.ReadFile(os.Args[1] + "/definitions.yml")
 			data = string(bytes)
 			if IsNotNil(err) {
-				log.Fatalf("cannot open declaration file %+v \n"  , err)
+				log.Fatalf("cannot open declaration file %+v \n", err)
 			}
-			fmt.Println("Definitions file founded")
+			log.Printf("Definitions file founded")
 			break
 		}
 	}
-
 	t := Definition{}
 	err = yaml.Unmarshal([]byte(data), &t)
 	if IsNotNil(err) {
 		log.Fatalf("error: %v", err)
 	}
-	fmt.Printf("--- t:\n%v\n\n", t)
-	for toolName, defs := range t.Definitions{
-		log.Printf("writing ...  %s \n" , toolName)
-
-		//err = os.Remove(defs.Path)
-		//if IsNotNil(err) {
-		//	fmt.Printf(" cannot remove path file due an error %v \n", err)
-		//	return
-		//}
-
-		log.Printf("path: %s template: %s" , defs.Path , defs.Template)
-		err = os.Symlink( args[1] + defs.Template , defs.Path)
+	for toolName, defs := range t.Definitions {
+		log.Printf("writing ...  %s \n", toolName)
+		if !FileNotExists(defs.Path) {
+			err = os.Remove(defs.Path)
+			if IsNotNil(err) {
+				log.Fatalf(" cannot remove path file due an error %v \n", err)
+			}
+		}
+		log.Printf("path: %s template: %s", defs.Path, defs.Template)
+		err = os.Symlink(args[1]+defs.Template, defs.Path)
 		if IsNotNil(err) {
-			fmt.Printf("cannot symlink due an error %v \n", err)
+			log.Printf("cannot symlink due an error %v \n", err)
 			return
 		}
-		fmt.Printf("tool configured ...  %s \n" , toolName)
+		log.Printf("tool configured ...  %s \n", toolName)
 	}
 }
 
-func IsNotNil(value error) bool{
+func IsNotNil(value error) bool {
 	return value != nil
 }
 
 func NotEmptyString(arg string) bool {
 	return len(strings.TrimSpace(arg)) > 0
+}
+
+func FileNotExists(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
 }
--- main_test.go
+++ main_test.go
@@ -1,9 +1,35 @@
 package main
 
-import ( "testing")
-func TestNotEmptyString(t *testing.T){
-	args := []string{""  , "path",  "/my/path"}
-	for _, arg := range args{
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestNotEmptyString(t *testing.T) {
+	args := []string{"", "path", "/my/path"}
+	for _, arg := range args {
 		NotEmptyString(arg)
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	pathStr := "/testFile.txt"
+	tempDir := os.TempDir()
+	err := ioutil.WriteFile(tempDir+pathStr, []byte("Hello"), 07555)
+	defer os.Remove(tempDir + pathStr)
+	if err != nil {
+		t.Fatalf("unexpected error while trying to test file exists %+v", err)
+	}
+	if FileNotExists(tempDir + pathStr) {
+		t.Fatalf("File was suppoused to exist")
+	}
+}
+
+func TestFileNotExists(t *testing.T) {
+	pathStr := "/zero"
+	tempDir := os.TempDir()
+	if !FileNotExists(tempDir + pathStr) {
+		t.Fatalf("File was suppoused to exist")
 	}
 }
