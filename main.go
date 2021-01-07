aca0205fb332b092b328398ffd14a9df0a2adfdb
Eduardo Villaseñor <evillasrmx@gmail.com>
2021-01-07 01:22:57 -0600

    Better vim file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -109,4 +110,8 @@ func FailIfError(text string, err error) {
 	if IsNotNil(err) {
 		log.Fatalf(text, err)
 	}
+}
+
+func unused() {
+	fmt.Println("Hello")
 }
